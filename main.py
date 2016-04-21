31a7eeb95ceb3ae2a3bc77fc83574b630ef383a0
Your Name <you@example.com>
2016-04-21 14:30:11 +0300

    pre ready

--- main.py
+++ main.py
@@ -12,14 +12,25 @@ from kivy.core.window import Window
 
 
 BOARD_SIZE = 8
-TEXTURE_EMPTY = Image(source='empty.png').texture
-TEXTURE_BLACK = Image(source='black.png').texture
-TEXTURE_WHITE = Image(source='white.png').texture
 
 CELL_STATE_WHITE = 'white'
 CELL_STATE_BLACK = 'black'
 CELL_STATE_EMPTY = 'empty'
-CELL_STATES = [CELL_STATE_WHITE, CELL_STATE_BLACK, CELL_STATE_EMPTY]
+CELL_STATE_STEP = 'step'
+
+CELL_STATES = [
+    CELL_STATE_WHITE,
+    CELL_STATE_BLACK,
+    CELL_STATE_EMPTY,
+    CELL_STATE_STEP
+]
+
+CELLS_TEXTURES = {
+    CELL_STATE_WHITE: Image(source='icons/white.png').texture,
+    CELL_STATE_BLACK: Image(source='icons/black.png').texture,
+    CELL_STATE_EMPTY: Image(source='icons/empty.png').texture,
+    CELL_STATE_STEP: Image(source='icons/step.png').texture,
+}
 
 PLAYER_WHITE = '1'
 PLAYER_BLACK = '2'
@@ -57,57 +68,92 @@ class MakeMoveCommand(Command):
     def __init__(self, cell):
         super(MakeMoveCommand, self).__init__()
         self.cell = cell
+        self.game = ReversiGame()
+        self.previous_states = []
 
     def execute(self):
-        game = ReversiGame()
-        game.toggle_player()
-        self.cell.state = PLAYERS_CELLS[game.current_player]
+        self.game.board.coords_state(self.cell.coord_x, self.cell.coord_y, PLAYERS_CELLS[self.game.current_player])
 
+        win_cells = self.game.get_win_cells(self.cell)
+        self.previous_states = [(i.coord_x, i.coord_y, i.state)for i in win_cells]
+
+        for win_cell in win_cells:
+            self.game.board.coords_state(win_cell.coord_x, win_cell.coord_y, self.cell.state)
+
+        self.game.toggle_player()
+        self.game.get_next_cells()
         HISTORY.append(self)
 
     def cancel(self):
-        game = ReversiGame()
-        game.toggle_player()
-        self.cell.state = CELL_STATE_EMPTY
+        self.game.board.coords_state(self.cell.coord_x, self.cell.coord_y, CELL_STATE_EMPTY)
+
+        for coord_x, coord_y, state in self.previous_states:
+            self.game.board.coords_state(coord_x, coord_y, state)
+
+        self.game.toggle_player()
+
 
 
 class BoardCell(Image):
     state = OptionProperty(CELL_STATE_EMPTY, options=CELL_STATES)
 
     def __init__(self, **kwargs):
-        super(BoardCell, self).__init__(**kwargs)
-
         self.coord_x = kwargs.get('coord_x', 0)
         self.coord_y = kwargs.get('coord_y', 0)
+
+        super(BoardCell, self).__init__(**kwargs)
 
         self.width = 128
         self.height = 128
 
-        self.texture = TEXTURE_EMPTY
+        self.texture = CELLS_TEXTURES[CELL_STATE_EMPTY]
 
-    def get_coords(self):
-        return self.coord_x, self.coord_y
+    def __unicode__(self):
+        return 'cell[%s,%s|%s]' % (self.coord_x, self.coord_y, self.state)
+
+    def __str__(self):
+        return 'cell[%s,%s|%s]' % (self.coord_x, self.coord_y, self.state)
 
     def on_state(self, instance, value):
-        if value == CELL_STATE_EMPTY:
-            self.texture = TEXTURE_EMPTY
-        elif value == CELL_STATE_WHITE:
-            self.texture = TEXTURE_WHITE
-        elif value == CELL_STATE_BLACK:
-            self.texture = TEXTURE_BLACK
+        self.texture = CELLS_TEXTURES[value]
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
-            command = MakeMoveCommand(self)
-            command.execute()
+            if self.state == CELL_STATE_STEP:
+                command = MakeMoveCommand(self)
+                command.execute()
 
 
 class Board(GridLayout):
     def coords(self, x, y):
-        return self.children[(x - 1) + BOARD_SIZE*(y - 1)]
+        try:
+            cell = self.coords_cells[x-1][y-1]
+            return cell
+        except IndexError:
+            print('[%s, %s] - missing' % (x, y))
+
+    def coords_state(self, x, y, state):
+        cell = self.coords(x, y)
+
+        cell.state = state
+
+        for i in CELL_STATES:
+            if i == state:
+                self.cells[state].append(cell)
+            else:
+                if cell in self.cells[i]:
+                    self.cells[i].remove(cell)
 
     def __init__(self, **kwargs):
         super(Board, self).__init__(**kwargs)
+
+        self.coords_cells = []
+        self.cells = {
+            CELL_STATE_WHITE: [],
+            CELL_STATE_BLACK: [],
+            CELL_STATE_STEP: [],
+            CELL_STATE_EMPTY: [],
+        }
 
         self.cols = BOARD_SIZE
         self.rows = BOARD_SIZE
@@ -140,19 +186,23 @@ class ReversiGame(object):
 
     def compose(self, root):
         self.current_player = PLAYER_WHITE
+        self.next_player = PLAYER_BLACK
 
         self.board = Board()
         self.board.padding = 10
 
         for x in range(BOARD_SIZE):
+            self.board.coords_cells.append([])
+
             for y in range(BOARD_SIZE):
                 broad_cell = BoardCell(coord_x=x+1, coord_y=y+1)
+                self.board.coords_cells[x].append(broad_cell)
                 self.board.add_widget(broad_cell)
 
-        self.board.coords(BOARD_SIZE/2, BOARD_SIZE/2).state = CELL_STATE_WHITE
-        self.board.coords(BOARD_SIZE/2 + 1, BOARD_SIZE/2 + 1).state = CELL_STATE_WHITE
-        self.board.coords(BOARD_SIZE/2, BOARD_SIZE/2 + 1).state = CELL_STATE_BLACK
-        self.board.coords(BOARD_SIZE/2 + 1, BOARD_SIZE/2).state = CELL_STATE_BLACK
+        self.board.coords_state(BOARD_SIZE / 2, BOARD_SIZE / 2, state=CELL_STATE_WHITE)
+        self.board.coords_state(BOARD_SIZE / 2 + 1, BOARD_SIZE / 2 + 1, state=CELL_STATE_WHITE)
+        self.board.coords_state(BOARD_SIZE / 2, BOARD_SIZE / 2 + 1, state=CELL_STATE_BLACK)
+        self.board.coords_state(BOARD_SIZE / 2 + 1, BOARD_SIZE / 2, state=CELL_STATE_BLACK)
 
         root.add_widget(self.board)
 
@@ -161,6 +211,7 @@ class ReversiGame(object):
 
         self.label_player1 = Label(text='Player 1', size_hint_x=.4)
         self.label_player2 = Label(text='Player 2', size_hint_x=.4)
+
         self.btn_undo = Button(text='Undo', size_hint_x=.2)
         self.btn_undo.bind(on_press=self.undo_handler)
 
@@ -170,18 +221,99 @@ class ReversiGame(object):
 
         root.add_widget(self.scores)
 
+        self.get_next_cells()
+
     def undo_handler(self, instance):
         command = UndoCommand()
         command.execute()
+
+    def get_paths(self, cell):
+        """
+        game paths for cell
+        """
+        x = cell.coord_x
+        y = cell.coord_y
+
+        x_right = range(x+1, BOARD_SIZE+1)
+        x_left = range(x-1, 0, -1)
+
+        y_up = range(y-1, 0, -1)
+        y_down = range(y+1, BOARD_SIZE+1)
+
+        return [
+            [(x, i) for i in y_up],
+            [(x, i) for i in y_down],
+
+            [(i, y) for i in x_left],
+            [(i, y) for i in x_right],
+
+            [(i, j) for i, j in zip(x_right, y_up)],
+            [(i, j) for i, j in zip(x_right, y_down)],
+
+            [(i, j) for i, j in zip(x_left, y_up)],
+            [(i, j) for i, j in zip(x_left, y_down)],
+        ]
+
+    def reset_next_cells(self):
+        cells = [(i.coord_x, i.coord_y)for i in self.board.cells[CELL_STATE_STEP]]
+        for x, y in cells:
+            self.board.coords_state(x, y, CELL_STATE_EMPTY)
+
+    def get_win_cells(self, cell):
+        self.reset_next_cells()
+        cells = []
+
+        for path in self.get_paths(cell):
+            next_cell = self.check_path(path)
+
+            if next_cell:
+                for x, y in path:
+                    cells.append(self.board.coords(x, y))
+
+                    if next_cell.coord_x == x and next_cell.coord_x:
+                        break
+
+        return cells
+
+    def check_path(self, path):
+        """
+        return cell if have step on path
+        """
+        states_count = 0
+        next_player_state = PLAYERS_CELLS[self.next_player]
+        current_player_state = PLAYERS_CELLS[self.current_player]
+
+        for x, y in path:
+            cell = self.board.coords(x, y)
+
+            if cell.state != next_player_state and states_count == 0:
+                break
+
+            if cell.state != next_player_state and states_count > 0:
+                return cell
+
+            if cell.state == next_player_state:
+                states_count += 1
+
+    def get_next_cells(self):
+        """
+        mark all next step cells
+        """
+        current_player_state = PLAYERS_CELLS[self.current_player]
+        next_player_state = PLAYERS_CELLS[self.next_player]
+
+        for cell in self.board.cells[current_player_state]:
+            for path in self.get_paths(cell):
+                next_cell = self.check_path(path)
+
+                if next_cell:
+                    self.board.coords_state(next_cell.coord_x, next_cell.coord_y, CELL_STATE_STEP)
 
     def calculate_score(self):
         pass
 
     def toggle_player(self):
-        if self.current_player == PLAYER_WHITE:
-            self.current_player = PLAYER_BLACK
-        else:
-            self.current_player = PLAYER_WHITE
+        self.current_player, self.next_player = self.next_player, self.current_player
 
 
 class ReversiApp(App):
